2505978291a9eac59a533f80262012d21c07e0e5
Natan <natan-moreira1@hotmail.com>
2024-03-31 21:03:04 -0300

    feat: adjust card load

--- src/components/chat/cards/card-load.tsx
+++ src/components/chat/cards/card-load.tsx
@@ -1,17 +1,28 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { ForwardedRef, forwardRef } from "react";
 
 type CardLoadProps = {
   type: "IA" | "USER";
 };
 
-export function CardLoad({ type }: CardLoadProps) {
-  return (
-    <Skeleton
-      className={cn(
-        `rounded-l-[8px]  max-w-2xl`,
-        type === "IA" ? "rounded-br-[8px]" : "rounded-bl-[8px]"
-      )}
-    />
-  );
-}
+const CardLoad = forwardRef(
+  ({ type }: CardLoadProps, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <div className="flex gap-2" ref={ref}>
+        <Skeleton
+          className={cn(`bg-slate-200 size-6 md:size-10 rounded-full`)}
+        />
+        <Skeleton
+          className={cn(
+            `h-28 flex-1 flex gap-2 bg-slate-200 rounded-l-[8px]  max-w-2xl`,
+            type === "IA" ? "rounded-br-[8px]" : "rounded-bl-[8px]"
+          )}
+        />
+      </div>
+    );
+  }
+);
+
+CardLoad.displayName = "CardLoad";
+export { CardLoad };
